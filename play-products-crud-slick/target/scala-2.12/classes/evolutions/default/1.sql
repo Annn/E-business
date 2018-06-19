ea44762e3c83b18a20b7f216cb0a349f5f9e2b87
annn <avasyutina@gmail.com>
2018-06-19 12:49:33 +0200

    tables, added bootstrap to views

--- play-products-crud-slick/target/scala-2.12/classes/evolutions/default/1.sql
+++ play-products-crud-slick/target/scala-2.12/classes/evolutions/default/1.sql
@@ -13,50 +13,58 @@ create table "product" (
   foreign key(category) references category(id)
 );
 
-create table "user" {
+create table "user" (
   "id" integer not null primary key autoincrement,
   "fullName" varchar not null
-};
+);
 
-create table "cart" {
+create table "cart" (
   "id" integer not null primary key autoincrement,
   "user" integer not null,
   foreign key(user) references User(id)
-};
+);
 
-create table "productcart" {
+create table "productcart" (
   "id" integer not null primary key autoincrement,
   "cart" integer not null,
   "product" integer not null,
   "number" integer not null,
   foreign key(cart) references Cart(id),
   foreign key(product) references product(id)
-};
+);
 
-create table "opinion" {
+create table "opinion" (
   "id" integer not null primary key autoincrement,
   "product" integer not null,
   "opin" text not null,
   foreign key(product) references product(id)
-};
+);
 
-create table "order" {
+create table "order" (
   "id" integer not null primary key autoincrement,
   "cart" integer not null,
   "order_date" varchar not null,
   "address" varchar not null,
   "cost" float not null,
-  foreign key(product) references product(id)
-};
+  foreign key(cart) references cart(id)
+);
 
 
 
 # --- !Downs
+drop table if exists "product" ;
+drop table if exists "category" ;
+drop table if exists "user" ;
+drop table if exists "cart" ;
+drop table if exists "productcart" ;
+drop table if exists "product" ;
+drop table if exists "opinion" ;
 
-drop table "product" if exists;
-drop table "category" if exists;
-drop table "user" if exists;
-drop table "cart" if exists;
-drop table "productcart" if exists;
-drop table "order" if exists;
-drop table "opinion" if exists;
+
+-- drop table "product" if exists;
+-- drop table "category" if exists;
+-- drop table "user" if exists;
+-- drop table "cart" if exists;
+-- drop table "productcart" if exists;
+-- drop table "order" if exists;
+-- drop table "opinion" if exists;
